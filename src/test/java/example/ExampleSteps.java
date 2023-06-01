95c879052ccd22cd1662f8e9cf6823470791ebe2
Vlad.S <70371590+VladSoro@users.noreply.github.com>
2023-06-01 16:40:51 +0300

    Test for Home Page

--- src/test/java/example/ExampleSteps.java
+++ src/test/java/example/ExampleSteps.java
@@ -1,5 +1,7 @@
 package example;
 
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,6 +14,11 @@ import org.openqa.selenium.WebElement;
 public class ExampleSteps {
 
     WebDriver driver = new ChromeDriver();
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
 
     @Given("I open wolt homepage")
@@ -34,5 +41,65 @@ public class ExampleSteps {
         Thread.sleep(1000);
         WebElement WoltBtn =driver.findElement(By.id("lottie"));
         WoltBtn.click();
+    }
+
+    @Given("I open wolt homepage and accept cookie")
+    public void iAmOnTheWoltHomePageAndAcceptCookie() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "D:\\qa\\chromedriver.exe");
+        driver.get("https://wolt.com/lt/ltu");
+        driver.manage().window().setSize(new Dimension(1526, 814));
+        WebElement AcceptButton = driver.findElement(By.xpath("//html/body/div[5]/div/div/div/button[2]"));
+        AcceptButton.click();
+        Thread.sleep(1000);
+    }
+
+    @When("I click wolt banner")
+    public void iClickWoltBanner() throws InterruptedException {
+        WebElement WoltBtn = driver.findElement(By.cssSelector("#lottie > svg"));
+        WoltBtn.isDisplayed();
+        WoltBtn.click();
+        Thread.sleep(1000);
+    }
+
+    @And("I see Home page")
+    public void iSeeHomePage() throws InterruptedException {
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        Thread.sleep(1000);
+    }
+
+    @Then("I see main page")
+    public void iSeeMainPage() throws InterruptedException {
+        String url = driver.getCurrentUrl();
+        System.out.println(url);
+        Thread.sleep(1000);
+    }
+
+    @When("I click Log in button")
+    public void iClickLogInButton() {
+        WebElement LoginBtn = driver.findElement(By.cssSelector(".sc-eda0895a-3"));
+        LoginBtn.isDisplayed();
+        LoginBtn.click();
+    }
+
+    @Then("I see 'Create an account or log in' pop-up")
+    public void iSeePopUp() throws InterruptedException {
+        Thread.sleep(1000);
+        WebElement LoginPopup = driver.findElement(By.cssSelector(".sc-b8df2b91-0 > svg"));
+        LoginPopup.isDisplayed();
+    }
+
+    @When("I close pop-up")
+    public void iClosePopUp() {
+        WebElement LoginPopup = driver.findElement(By.cssSelector(".sc-b8df2b91-0 > svg"));
+        LoginPopup.click();
+    }
+
+
+    @When("I click 'Sign up' button")
+    public void iClickSignUpButton() {
+        WebElement SignUpBtn = driver.findElement(By.cssSelector(".fHmvvV"));
+        SignUpBtn.isDisplayed();
+        SignUpBtn.click();
     }
 }
--- src/test/resources/scenarios/example.feature
+++ src/test/resources/scenarios/example.feature
@@ -1,5 +1,31 @@
-Feature: Wolt Feature
-  Scenario: Home page test
+Feature: Wolt Home Page
+
+
+  Scenario: Accept cookies
     Given I open wolt homepage
     When I see cookie banner
     Then I click 'Accept' button
+    And I see Home page
+
+
+
+  Scenario: Open main page
+     Given I open wolt homepage and accept cookie
+     When I click wolt banner
+     Then I see main page
+
+
+  Scenario: checking registration and authorization buttons
+    Given I open wolt homepage and accept cookie
+    When I click Log in button
+    Then I see 'Create an account or log in' pop-up
+    When I close pop-up
+    And I see Home page
+    When I click 'Sign up' button
+    Then I see 'Create an account or log in' pop-up
+
+
+  Scenario: checking registration and authorization buttons
+
+
+
